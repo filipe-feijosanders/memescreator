d71660cb7bd5af0b6533599c017dac0b150b2701
filipesanders-poatek <filipe.sanders@poatek.com>
2026-01-02 12:54:52 -0300

    fix init koin for web

--- composeApp/src/webMain/kotlin/com/example/memeapp/main.kt
+++ composeApp/src/webMain/kotlin/com/example/memeapp/main.kt
@@ -2,10 +2,12 @@ package com.example.memeapp
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.example.memeapp.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport {
+        initKoin()
         App()
     }
 }
